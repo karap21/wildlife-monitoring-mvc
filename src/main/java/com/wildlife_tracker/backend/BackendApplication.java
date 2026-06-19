d78054a6cf06ac0f70fe66d5183e5b895689dbbb
karap21 <nielsembirinng@gmail.com>
2026-06-19 09:43:29 +0700

    pleasee

---
+++ src/main/java/com/wildlife_tracker/backend/BackendApplication.java
@@ -0,0 +1,12 @@
+package com.wildlife_tracker.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+    public static void main(String[] args) {
+        // Ini adalah tombol starter untuk menyalakan seluruh sistem aplikasi Anda
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
